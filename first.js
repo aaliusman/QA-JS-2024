3e784c19c96c020a131bc39bbc41b1235bd2446a
Muhammad Khaleed <mk@Muhammads-MBP.hsd1.pa.comcast.net>
2024-02-11 15:32:36 -0500

    Javascript basics

---
+++ first.js
@@ -0,0 +1,116 @@
+console.log('Hello World')
+
+// commonly use variable type we use in javascript is let and const
+// when we create variales we shold have dataype and variable type
+
+// dataype is string anything in the quote
+let firstName = 'John'
+console.log(firstName);
+
+firstName = 'Muhammad';
+console.log(firstName);
+
+const lastName = 'Usman'
+console.log(lastName)
+// lastName = 'Salam';
+
+let text = "Usman's number 215000"
+console.log(text)
+
+//datatype integer
+let myAge = 50
+console.log(myAge)
+
+//two common types of writing variables camel case and snake case
+let numOfSchool = 45;
+console.log(numOfSchool)
+
+let num_of_school = 45;
+console.log(num_of_school)
+
+// Arithmatic operators  + - * / %
+
+let sales = 50 + 100;
+console.log(sales)
+
+sales = sales + 100;
+console.log(sales)
+
+sales = sales + 100
+console.log(sales)
+
+sales = sales - 200;
+console.log(sales)
+
+let newSale = 200
+
+newSale = newSale * 2
+console.log(newSale)
+
+newSale = newSale / 4
+console.log(newSale)
+
+let remainder = 5 % 2
+console.log(remainder)
+
+remainder = 6 % 2
+console.log(remainder)
+
+remainder = 14 % 3
+console.log(remainder)
+
+remainder = 27 % 7
+console.log(remainder)
+
+remainder = 42 / 7
+console.log(remainder)
+
+remainder = 42 % 7
+console.log(remainder)
+
+
+// variable with integer and string
+let intStr = 'My favorite number is 16'
+console.log(intStr)
+
+// this is also called concatenation 
+// + can be use for addition and concatenation
+let sumOfTwo = 7 + 9
+intStr = 'My favorite number is ' - sumOfTwo
+console.log(intStr)
+
+let text1 = 'I am going to'
+let city = 'Dhaka'
+
+console.log(text1 + ' ' + city)
+
+
+let fName = 'Usman'
+
+let lName = "Muhammad"
+
+let cityName = 'Zurich'
+
+console.log(fName + ' ' + lName + ', from ' + cityName)
+
+let text2 = 'Pizza'
+let text3 = 'Sandwich'
+let storeName = "Domino's"
+
+console.log(text2 + ' ' + text3 + ', from ' + storeName)
+
+let sumOfThreeNumbers = 'Hello ' + 9 + '4' + 6
+console.log(sumOfThreeNumbers)
+//Hello 946
+
+let sumOfThreeNumber = 5 + 9 + 6 + '4'
+console.log(sumOfThreeNumber)
+//204
+
+sumOfThreeNumber = '5' + 9 + 6 + '4'
+console.log(sumOfThreeNumber)
+//5964
+
+//Static variable and dynamic variable 
+// static doens't change and dynamic chages
+
